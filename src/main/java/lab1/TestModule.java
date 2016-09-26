c1a3fcdd205c229b02f8e20acb1c637594e9485a
Dmytro Kytsmen <dmitrokytsmen@gmail.com>
2016-09-26 10:15:04 +0300

    Started lab 1

---
+++ src/main/java/lab1/TestModule.java
@@ -0,0 +1,14 @@
+package lab1;
+
+/**
+ * Created by Dmytro on 25.09.2016.
+ */
+public class TestModule {
+    @Override
+    public String toString() {
+        return "TestModule, version 1!";
+    }
+
+    public TestModule() {
+    }
+}
